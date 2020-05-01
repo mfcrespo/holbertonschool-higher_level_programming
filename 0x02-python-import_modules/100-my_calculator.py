c3e90d3b092025906a7c575ef39f2b009f354502
mfcrespo <mafe_crespo@hotmail.com>
2020-05-01 05:08:41 +0000

    Add task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    oper = sys.argv[2]
-    if oper != "+" and oper != "-" and oper != "*" and oper != "/":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    if len(argv) - 1 != 3:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    if argv[2] == '+':
+        print("{} + {} = {:d}"
+              .format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '-':
+        print("{} - {} = {:d}"
+              .format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '*':
+        print("{} * {} = {:d}"
+              .format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '/':
+        print("{} / {} = {:d}"
+              .format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if oper == "+":
-        print("{} {} {} = {}".format(a, oper, b, add(a, b))) 
-    if oper == "-":
-        print("{} {} {} = {}".format(a, oper, b, sub(a, b)))
-    if oper == "*":
-        print("{} {} {} = {}".format(a, oper, b, mul(a, b)))
-    if oper == "/":
-        print("{} {} {} = {}".format(a, oper, b, div(a, b)))
+        exit(1)
