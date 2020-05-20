915c72a5298727bb40fdca100512b2b62043b293
mfcrespo <mafe_crespo@hotmail.com>
2020-05-20 02:32:50 +0000

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Documentation for defines a square"""
+
+
+class Square:
+    """Class defines a square"""
+
+    def __init__(self, size):
+        """Gives the initial size of the new square object
+        Args:
+            size (int): the size of the square once instance is created
+        """
+        self.__size = size
