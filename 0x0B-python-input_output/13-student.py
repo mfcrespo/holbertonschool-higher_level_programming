cec548f6c32afe315a52457c77d19902c0c0ed0b
mfcrespo <mafe_crespo@hotmail.com>
2020-06-08 05:01:31 +0000

    Add task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" My Module for creating a Student class
+"""
+
+
+class Student():
+    """ Creates a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes a student
+        Args:
+            first_name (str): Student's first name
+            last_name (str): Student's last name
+            age (int): age of Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns dictionary description of the instance
+        Args:
+            attrs (list): List of strings
+                          to check for in the dict representation
+        """
+        my_dict = {}
+        new_dict = self.__dict__
+        if attrs is None:
+            return new_dict
+        for key, value in new_dict.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
+
+    def reload_from_json(self, json):
+        """ Reassembles class attributes from json text
+        Args:
+            json (dict): dict to parse through
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
