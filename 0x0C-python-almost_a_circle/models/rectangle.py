180848781c957d11c2529a19a223c153abaee886
mfcrespo <mafe_crespo@hotmail.com>
2020-06-09 05:32:35 +0000

    Add task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,9 +13,9 @@ class Rectangle(Base):
         Args:
             width (int): width of the rectangle
             height (int): height of the rectangle
-            x (int): how much to move the rectangle when printing in x
-            y (int): how much to move the rectangle when printing in y
-            id (int): number of instances of rectangles
+            x (int): the x coordinate of the rectangle
+            y (int): the y coordinate of the rectangle
+            id (int): number of rectangle
         """
 
         super().__init__(id)
@@ -23,3 +23,109 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @staticmethod
+    def size(var_size, value):
+        """ Validates and handles all error messages of height or width
+        Args:
+            var_size (str): the variable name of size
+            value (int): the value associated with var_size
+        """
+
+        size = ["width", "height"]
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(var_size))
+        if var_size in size and value <= 0:
+            raise ValueError("{} must be > 0".format(var_size))
+
+    @property
+    def width(self):
+        """Getter of the width attribute
+        Returns:
+            the width of the instance
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width attribute
+        Args:
+            value (int): value to set to width
+        """
+
+        self.size("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height attribute
+        Returns:
+            the height of the instance
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height attribute
+        Args:
+            value (int): the value to set the height to
+        """
+
+        self.size("height", value)
+        self.__height = value
+
+    @staticmethod
+    def coordinate(var_coord, value):
+        """ Validates and handles all error messages
+        Args:
+            var_coord (str): the variable name
+            value (int): the value associated with var_coord
+        """
+
+        coord = ["x", "y"]
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(var_coord))
+        if var_coord in coord and value < 0:
+            raise ValueError("{} must be >= 0".format(var_coord))
+
+    @property
+    def x(self):
+        """Getter for the x attribute
+        Returns:
+            the x attribute of the instance
+        """
+
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for the x attribute
+        Args:
+            value (int): the value to set the x attribute to
+        """
+
+        self.coordinate("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for the y attribute
+        Returns:
+            the y attribute of the instance
+        """
+
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter of the y attribute
+        Args:
+            value (int): the value to set the y attribute to
+        """
+
+        self.coordinate("y", value)
+        self.__y = value
