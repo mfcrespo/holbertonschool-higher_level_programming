093d18b9b6dac5751fd57ed8d064cfc3c1944cab
mfcrespo <mafe_crespo@hotmail.com>
2020-06-10 00:35:52 +0000

    Add task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-""" My Module for creating Square class that inherits from Rectangle
-"""
+"""Documentation for a Square class"""
+
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ class Square that inherits from Rectangle
+
+    """Square class that inherits from the Rectangle class
+       which in turn inherited from the Base class
     """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initialize an instance of a square
+        """Instantiation of a Square object
         Args:
-            size (int): size of the square
-            x (int): how much to shift the square when printing
-            y (int): how much to shift the square when printing
-            id (int): number of instances of squares
+            size (int): the size of the square
+            x (int): the x coordinate
+            y (int): the y coordinate
+            id (int): the id of the object
         """
 
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Overloading the __str__ method to return certain string"""
+
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.width)
