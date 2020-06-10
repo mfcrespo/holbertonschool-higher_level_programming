9c208b986b26b3dbd4c74b98e76ee3de1d04b226
mfcrespo <mafe_crespo@hotmail.com>
2020-06-10 03:32:22 +0000

    Add test base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,7 @@
 Unittest por Base class
 """
 import unittest
+import json
 from models.base import Base
 
 
@@ -40,9 +41,6 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b3.id, 3)
         self.assertEqual(b4.id, 4)
 
-if __name__ == '__main__':
-    unittest.main()
-
     def test_reassign_id(self):
         """ Test if id is correct in all instances after reassigning it
         """
@@ -61,3 +59,31 @@ if __name__ == '__main__':
             self.assertEqual(instances[position].id, i)
             position += 1
 
+    def test_to_json_string(self):
+        """ Test if the to_json_string method works
+        """
+
+        json_string = Base.to_json_string([{'x': 5}])
+        json_string_two = Base.to_json_string([{'x': 5}, {'y': 3}])
+
+        self.assertEqual(json_string, '[{"x": 5}]')
+        self.assertEqual(json_string_two, '[{"x": 5}, {"y": 3}]')
+
+    def test_to_json_string_none(self):
+        """ Test what happens when to_json_string is passed None
+        """
+
+        json_string = Base.to_json_string(None)
+
+        self.assertEqual(json_string, '[]')
+
+    def test_to_json_string_empty_list(self):
+        """ Test what happens when to_json_string is passed an empty list
+        """
+
+        json_string = Base.to_json_string([])
+
+        self.assertEqual(json_string, '[]')
+
+if __name__ == '__main__':
+    unittest.main()
