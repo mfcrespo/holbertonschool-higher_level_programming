f72dba3c004acf96b1d6bf459bc56af54d445e25
mfcrespo <mafe_crespo@hotmail.com>
2020-06-10 02:09:08 +0000

    Add task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ My Module for creating a base class
 """
+import json
 
 
 class Base:
@@ -25,3 +26,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ that returns the JSON string representation of list_dictionaries
+        Args:
+            list_dictionaries (list): list of representations of an instances
+        """
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
