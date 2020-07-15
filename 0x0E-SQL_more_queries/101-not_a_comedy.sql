860968ba63d04e3d8aa3ff64dd16831eaa0986b3
mfcrespo <mafe_crespo@hotmail.com>
2020-07-15 04:25:57 +0000

    Add task 18

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,14 @@
+-- Write a script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use a maximum of two SELECT statement
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN
+      (SELECT tv_shows.title
+       FROM tv_genres INNER JOIN tv_show_genres
+        ON tv_genres.name = 'Comedy' and tv_genres.id = tv_show_genres.genre_id
+	 INNER JOIN tv_shows
+	  ON tv_shows.id = tv_show_genres.show_id)
+ORDER BY tv_shows.title ASC;
