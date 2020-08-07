e6d3c68d4931de30784af620ae38d4aa03e9dcf1
mfcrespo <mafe_crespo@hotmail.com>
2020-08-07 05:07:52 +0000

    Change comment

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Write a script that lists all states from the database hbtn_0e_0_usa
+Write a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument
 """
 
 import MySQLdb
