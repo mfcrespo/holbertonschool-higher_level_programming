0ac014bfd835a71423f02c23740929f2254cd923
mfcrespo <mafe_crespo@hotmail.com>
2020-08-08 15:39:42 +0000

    Fix ORDER BY

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
                  FROM cities INNER JOIN states ON cities.state_id = states.id \
-                 ORDER BY states.id ASC")
+                 ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
