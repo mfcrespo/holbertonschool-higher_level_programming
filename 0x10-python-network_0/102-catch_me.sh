3fe6301a92671d10d3ec6501483779ced8210c73
mfcrespo <mafe_crespo@hotmail.com>
2020-08-19 05:02:43 +0000

    Add task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me and makes the server respond with "You got me!"
-curl 0.0.0.0:5000/catch_me -sLX PUT -d "user_id=98" -H "Origin: DHKSchool"
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
