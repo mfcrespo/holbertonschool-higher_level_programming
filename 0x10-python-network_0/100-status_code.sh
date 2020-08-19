ded569ab040e37ecd303ae5b76054427c558b99e
mfcrespo <mafe_crespo@hotmail.com>
2020-08-19 04:52:58 +0000

    fix shellcheck

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script sends a request to a URL passed as an argument, displays only the status code of the response
-curl "$1" -sI -w '%{http_code}' -o /dev/null
+curl -s -I "$1" -o /dev/null -w '%{http_code}'
