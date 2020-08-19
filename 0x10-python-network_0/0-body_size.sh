366c7ba8004b424a93e5b51c92ad8df784b0ca56
mfcrespo <mafe_crespo@hotmail.com>
2020-08-19 00:33:33 +0000

    Delete a line of comment

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#script that takes in a URL, sends a request to that URL,
-#and displays the size of the body of the response
-curl -w '%{size_download}\n' -so /dev/null $1
+#script takes in a URL, sends a request to URL, displays the size of the body of the response
+curl -w '%{size_download}\n' -so /dev/null "$1"
