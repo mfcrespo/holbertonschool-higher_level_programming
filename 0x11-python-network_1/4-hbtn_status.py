13da760bc463952a71e61052c776c50af0d8d655
mfcrespo <mafe_crespo@hotmail.com>
2020-08-24 23:56:52 +0000

    Add task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches https://intranet.hbtn.io/status
+"""
+import requests
+if __name__ == "__main__":
+    req = requests.get("http://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type:{}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
