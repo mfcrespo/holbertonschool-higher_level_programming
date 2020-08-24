af53402fdaeb530cc5ffb7b763f524170be4a143
mfcrespo <mafe_crespo@hotmail.com>
2020-08-24 16:52:02 +0000

    Add task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches https://intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        page = response.read()
+    print('Body response:')
+    print('\t- type: ', type(page))
+    print('\t- content: ', page)
+    print('\t- utf8 content: ', page.decode('utf-8'))
