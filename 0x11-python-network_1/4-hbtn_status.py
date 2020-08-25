a2844148574b92fe29a765b5bc3ab7e959412ac7
mfcrespo <mafe_crespo@hotmail.com>
2020-08-25 00:02:36 +0000

    Fix spaces

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,9 @@
 Write a Python script that fetches https://intranet.hbtn.io/status
 """
 import requests
+
 if __name__ == "__main__":
     req = requests.get("http://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type:{}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    print("\t- content:{}".format(req.text))
