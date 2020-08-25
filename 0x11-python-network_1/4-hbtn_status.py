a611730503893d56b8bb3280d6e1a1e2cf58f3ea
mfcrespo <mafe_crespo@hotmail.com>
2020-08-25 00:16:28 +0000

    Fix https

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ Write a Python script that fetches https://intranet.hbtn.io/status
 import requests
 
 if __name__ == "__main__":
-    req = requests.get("http://intranet.hbtn.io/status")
+    req = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
     print("\t- content: {}".format(req.text))
