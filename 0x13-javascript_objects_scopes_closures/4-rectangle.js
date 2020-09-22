388c1db25274c2305ac2bcc2f5f3f872133073eb
mfcrespo <mafe_crespo@hotmail.com>
2020-09-21 22:12:52 -0500

    Add task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,31 +1,42 @@
 #!/usr/bin/node
-// Write a class Rectangle that defines a rectangle
+// Write a class Rectangle that defines a rectangle:
+// You must use the class notation for defining your class
+// The constructor must take 2 arguments: w and h
+// Initialize the instance attribute width with the value of w
+// Initialize the instance attribute height with the value of h
+// If w or h is equal to 0 or not a positive integer, create an empty object
+// Create an instance method called print() that prints the rectangle using
+// the character X
+// Create an instance method called rotate() that exchanges the width and the
+// height of the rectangle
+// Create an instance method called double() that multiples the width and the
+// height of the rectangle by 2
 
 module.exports = class Rectangle {
-	constructor (w, h) {
-	  if (w > 0 && h > 0) {
-	    this.width = w;
-	    this.height = h;
-	  }
-	}
-      
-	print () {
-	  for (let i = 0; i < this.height; i++) {
-	    for (let j = 0; j < this.width; j++) {
-	      process.stdout.write('X');
-	    }
-	    process.stdout.write('\n');
-	  }
-	}
-      
-	rotate () {
-	  let temp = this.height;
-	  this.height = this.width;
-	  this.width = temp;
-	}
-      
-	double () {
-	  this.width *= 2;
-	  this.height *= 2;
-	}
-      };
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
