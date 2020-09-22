0269cbe7d54f54456aece4a4d536da107a567cd6
mfcrespo <mafe_crespo@hotmail.com>
2020-09-21 21:57:03 -0500

    Fix indent

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -6,8 +6,8 @@
 // Initialize the instance attribute height with the value of h
 
 module.exports = class Rectangle {
-	constructor (w, h) {
-	  this.width = w;
-	  this.height = h;
-	}
-      };
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
